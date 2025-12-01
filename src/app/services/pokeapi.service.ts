2c030261ac6a68d206e82db519c75f689229a363
Marco Antonio Bahena Martinez <marcobahena2024@outlook.com>
2025-12-01 14:02:54 -0600

    Agregue el servicio para el endpoint de la pokeApi

--- src/app/services/pokeapi.service.ts
+++ src/app/services/pokeapi.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokeapiService {
 
-  constructor() { }
+    private apiUrl = 'https://pokeapi.co/api/v2/';
+  constructor( private http: HttpClient) { 
+  
+  }
+  getPokemon(id: string): Observable<any> {
+    // Obtiene la información de un pokemon específico por su ID o nombre
+    const url = `${this.apiUrl}pokemon/${id}`;
+    return this.http.get<any>(url);
+  }
 }
